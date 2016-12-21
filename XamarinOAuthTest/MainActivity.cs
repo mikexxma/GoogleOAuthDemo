df0e4e09be8675057187fcc6ddecd082414400bc
v-mxiaox <v-mxiaox@CNV-MXIAOX-W10>
2016-12-21 11:17:52 +0800

    add customer style

--- XamarinOAuthTest/MainActivity.cs
+++ XamarinOAuthTest/MainActivity.cs
@@ -3,16 +3,17 @@ using Android.Widget;
 using Android.OS;
 using Xamarin.Auth;
 using System;
+using Android.Views;
 
 namespace XamarinOAuthTest
 {
-    [Activity(Label = "XamarinOAuthTest", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "XamarinOAuthTest", MainLauncher = true, Icon = "@drawable/icon", Theme = "@android:style/Theme.NoTitleBar")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
+            //Window.RequestFeature(WindowFeatures.NoTitle);
             // Set our view from the "main" layout resource
             //SetContentView (Resource.Layout.Main);
 
